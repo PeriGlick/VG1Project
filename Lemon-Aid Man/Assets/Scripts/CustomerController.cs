7e6dfdd214019ebf43c7a95198c2958b662b7717
jennifer-jin-8 <j.jennifer@wustl.edu>
2023-02-24 00:04:38 -0600

    Update CustomerController.cs

--- Lemon-Aid Man/Assets/Scripts/CustomerController.cs
+++ Lemon-Aid Man/Assets/Scripts/CustomerController.cs
@@ -20,4 +20,12 @@ public class CustomerController : MonoBehaviour
         var standPosition = stand.transform.position + new Vector3(0, -1f, 0);
         transform.position = Vector3.MoveTowards(transform.position, standPosition, step);
     }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // If customer is hit by a player projectile (layer=7), customer disappears
+        if (other.gameObject.layer == 7) {
+            Destroy(gameObject);
+        }
+    }
 }
