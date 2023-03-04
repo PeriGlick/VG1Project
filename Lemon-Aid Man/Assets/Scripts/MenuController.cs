eb53586e681a12411b382c4db2b32ee423c6fd88
PeriGlick <77940128+PeriGlick@users.noreply.github.com>
2023-03-03 20:09:16 -0600

    Weapons menu code is up
    Will open and close within range by pressing E, will turn yellow within range, trying to fix graphical glitch of characters going above the menu

--- Lemon-Aid Man/Assets/Scripts/MenuController.cs
+++ Lemon-Aid Man/Assets/Scripts/MenuController.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    //public GameObject[] standMenuItems;
+    public GameObject StandMenu;
+    
+    public GameObject playerObject;
+    bool InRange = false;
+    SpriteRenderer sprite;
+    //Color highlight = Color.yellow;
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale= 1;
+
+        
+        hidePaused();
+        sprite = GetComponent<SpriteRenderer>();
+        //sprite.color = Color.white;
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if(InRange == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (Time.timeScale == 1)
+                {
+                    Time.timeScale = 0;
+                    showPaused();
+                    playerObject.SetActive(false);
+                }
+                else if (Time.timeScale == 0)
+                {
+                    Time.timeScale = 1;
+                    hidePaused();
+                    playerObject.SetActive(true);
+                }
+            }
+        }
     }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            InRange= true;
+            sprite.color = Color.yellow;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            InRange = false;
+            sprite.color = Color.white;
+           
+        }
+    }
+
+
+    public void showPaused()
+    {
+      
+
+        StandMenu.SetActive(true);
+
+    }
+
+    public void hidePaused()
+    {
+
+
+        StandMenu.SetActive(false);
+    }
+
 }
