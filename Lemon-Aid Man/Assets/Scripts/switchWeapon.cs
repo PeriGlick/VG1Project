0b9a77c0d9e91e7b9f253dff37468c97e4081f4b
PeriGlick <77940128+PeriGlick@users.noreply.github.com>
2023-03-31 20:58:54 -0500

    Working on Seed Spitter in Menu
    added a button and wrote a script. I figure here is where the duck kill unlock system fits in --> kill more to get a better weapon, spend the money on the stand - everything fits into place

---
+++ Lemon-Aid Man/Assets/Scripts/switchWeapon.cs
@@ -0,0 +1,29 @@
+using LemonAidMan;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class switchWeapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button seedSpitterButton;
+    public GameObject player;
+    public GameObject seedSpitterPrefab;
+    void Start()
+    {
+        seedSpitterButton.onClick.AddListener(changeWeaponSS);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeWeaponSS()
+    {
+        player.GetComponent<playerMovement>().projectilePrefab = seedSpitterPrefab;
+    }
+}
