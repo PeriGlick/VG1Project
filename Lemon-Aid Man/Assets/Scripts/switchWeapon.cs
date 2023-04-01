2a6b425376ec523868835e4b33c03d62bc8b30d1
PeriGlick <77940128+PeriGlick@users.noreply.github.com>
2023-03-31 21:03:56 -0500

    Adding duck kill requirements but needs more work/finishing
    Update Game.unity
    
    Update Game.unity

--- Lemon-Aid Man/Assets/Scripts/switchWeapon.cs
+++ Lemon-Aid Man/Assets/Scripts/switchWeapon.cs
@@ -11,19 +11,30 @@ public class switchWeapon : MonoBehaviour
     public Button seedSpitterButton;
     public GameObject player;
     public GameObject seedSpitterPrefab;
+    public DuckKillScript dks;
+    public GameObject DuckKillMonitor;
+    public int firstUnlock = 12;
     void Start()
     {
         seedSpitterButton.onClick.AddListener(changeWeaponSS);
+        seedSpitterButton.interactable = false;
+        DuckKillMonitor = GameObject.Find("DuckKillMonitor");
+        dks = DuckKillMonitor.GetComponent<DuckKillScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dks.duckKills >= firstUnlock)
+        {
+            seedSpitterButton.interactable = true;
+        }
     }
 
     public void changeWeaponSS()
     {
         player.GetComponent<playerMovement>().projectilePrefab = seedSpitterPrefab;
+        seedSpitterButton.interactable = false;
+
     }
 }
