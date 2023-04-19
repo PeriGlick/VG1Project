ff611e474892db4446273f547b849faed6688ea5
PeriGlick <77940128+PeriGlick@users.noreply.github.com>
2023-04-18 23:14:38 -0500

    Nerfed the lemonade splash to reduce spamming > longer reload time

--- Lemon-Aid Man/Assets/Scripts/LemonadeSplash.cs
+++ Lemon-Aid Man/Assets/Scripts/LemonadeSplash.cs
@@ -7,7 +7,7 @@ public class LemonadeSplash : MonoBehaviour
     private Rigidbody2D _rb;
     public float speed = 6f;
     public float liveTime = 0.75f;
-    public float weaponCoolDown = 1.5f;
+    public float weaponCoolDown = 2f;
     // Start is called before the first frame update
     void Start()
     {
